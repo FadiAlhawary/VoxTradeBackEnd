2fe304cdfed7421ed72aa75dbf7e98654c6db439
Fadi Al Hawary <f.alhawary@ids-fintech.com>
2026-04-20 12:39:50 +0300

    added wallet

---
+++ Models/DTO/WalletDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VoxTrade.Models.DTO
+{
+    public class WalletDto
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        public int? CurrencyId { get; set; }
+        [Column(TypeName = "numeric")]
+        public decimal Balance { get; set; } = 0;
+        [Column(TypeName = "numeric")]
+        public decimal AvailableBalance { get; set; } = 0;
+        [Column(TypeName = "numeric")]
+        public decimal ReservedBalance { get; set; } = 0;
+        public bool Status { get; set; } = true;
+        public DateTime UpdatedAt { get; set; }
+        public List<WalletHistoryDto> walletHistory { get; set; }
+    }
+}
